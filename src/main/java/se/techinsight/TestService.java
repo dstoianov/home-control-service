fbd2cddc6612cafd972f946bcd1b20955a821ff4
Denys Stoianov <denys.stoianov@raisin.com>
2021-01-23 19:57:30 +0100

    test new release

---
+++ src/main/java/se/techinsight/TestService.java
@@ -0,0 +1,13 @@
+package se.techinsight;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestService {
+
+    @GetMapping
+    public String test() {
+        return "OK";
+    }
+}
